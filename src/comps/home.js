212d7fcc3b43d479f6b57578a6766e660f46bbcd
Benorr91 <95618872+Benorr91@users.noreply.github.com>
2022-01-28 11:26:03 +0200

    aa

--- src/comps/home.js
+++ src/comps/home.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useRef, useState }
     from 'react';
-import {  useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 
 function Home(props) {
+    let re = new RegExp(/ab+c/, 'i')
     let params = useParams();
     let searchQ = useRef();
     let navigate = useNavigate();
@@ -20,13 +21,37 @@ function Home(props) {
 
     const doApi = async () => {
         let url = `https://www.omdbapi.com/?i=tt3896198&apikey=813ad757&s=${params.searchbyCat ? params.searchbyCat : "bank"}&y=${params.searchbyYear ? params.searchbyYear : ""}`
-        let resp = await axios.get(url)
-        setAr(resp.data.Search);
-        console.log(params.searchbyYear);
-        console.log(resp.data.Search);
+        try {
+            let resp = await axios.get(url)
+            setAr(resp.data.Search);
+            console.log(params.searchbyYear);
+            console.log(resp.data.Search);
+        } catch (error) {
+            alert("No Data With This name")
+            navigate("/")
+
+            if (error.response) {
+                // Request made and server responded
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+
+        }
+
+
     }
     const onBtnClickSearch = () => {
+        const value = re.exec(searchQ.current.value)
+        console.log(value);
         navigate("/search/" + searchQ.current.value);
+        searchQ.current.value = "";
     }
     const onBtnClickYear = (e) => {
         navigate("/year/" + e.target.innerHTML);
@@ -38,14 +63,14 @@ function Home(props) {
         console.log(e.target.value);
 
     }
-    const yearsAr = [];
-    const years=[1991,1978,2015,2020,1948,1999,2015];
+    const years = [1991, 1978, 2015, 2020, 1948, 1999, 2015];
     years.sort();
 
+    // const yearsAr = [];
     // for (let i = 2000; i <= 2021; i++) {
     //     yearsAr.push(i);
     // }
- 
+
     return (
         <div className='container' >
             <h1 className='h1Home my-3 display-3 fw-bold'>Vod Movies</h1>
@@ -61,26 +86,30 @@ function Home(props) {
                     {years.map((item, i) => {
                         return (
                             <button key={i} className="badge bg-primary text-wrap mx-1 text-light" onClick={onBtnClickYear}> {item}</button>
-                            
+
                         )
                     })}
                 </div>
 
             </nav>
-            <div className="row p-3">
-                {ar.map((item, i) => {
-                    return (
-                        <div key={i} className="card shadow p-3 m-3" style={{ width: " 18rem" }}>
-                            <img src={item.Poster == "N/A" ? "https://cdn.pixabay.com/photo/2021/12/02/12/17/happy-new-year-6840369__340.png" : item.Poster} className="card-img-top" alt={item.Title} />
-                            <div className="card-body">
-                                <h5 className="card-title">{item.Title}</h5>
-                
-                                <button value={item.imdbID} onClick={onBtnClickInfo} className="btn btn-primary" >More Info</button>
+
+
+            {ar ?
+                <div className="row p-3">
+                    {ar.map((item, i) => {
+                        return (
+
+                            <div key={i} className="card shadow p-3 m-3" style={{ width: " 18rem" }}>
+                                <img src={item.Poster == "N/A" ? "https://cdn.pixabay.com/photo/2021/12/02/12/17/happy-new-year-6840369__340.png" : item.Poster} className="card-img-top" alt={item.Title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.Title}</h5>
+
+                                    <button value={item.imdbID} onClick={onBtnClickInfo} className="btn btn-primary" >More Info</button>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </div>
+                        )
+                    })}
+                </div> : ""}
         </div>
     )
 }
